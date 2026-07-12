7b37d3b53172a1dc2b6f8458790b8749283a930e
maxx7747 <percept100l@gmail.com>
2026-07-12 12:08:50 +0530

    Created EcoSphere module

---
+++ custom_addons/ecosphere/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'EcoSphere ESG Management',
+    'version': '1.0',
+    'summary': 'ESG Management System',
+    'author': 'Team EcoSphere',
+    'category': 'Management',
+    'depends': ['base'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/menu.xml',
+    ],
+    'installable': True,
+    'application': True,
+}
